a098a5491fbcd19215e418fa56c328a97b998576
Nullises <texanico@gmail.com>
2017-08-31 23:34:38 -0400

    Materialize

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.set('view engine', 'ejs');
 //Definir carpetas estáticas a usar:
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/bower_components'));
 
 
 //Definir middlewares:
