f391a0f0765eb4c7a69ecf4a7d16d89e7bcf0965
Nullises <texanico@gmail.com>
2017-09-01 12:57:13 -0400

    CRUD: getAll

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ var models = require('../server/models/index');
 
 //Pagina inicial
 router.get('/', function(req, res, next) {
-  res.json({ message: 'API Initialized!'});
+  res.render('index', { title: 'Express' });
+});
+
+//Obtener todos los productos
+router.get('/getAll', function(req, res){
+  models.product.findAll({}).then(function(getAll) {
+    res.json(getAll);
+  });
 });
 
 //Comprobar conexión a DB
@@ -13,6 +20,5 @@ models.sequelize.authenticate().then(function(err) {
 }).catch(function (err) {
     console.log('Imposible conectarse a Base de Datos:', err);
 });
-
 
 module.exports = router;
