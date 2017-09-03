895d4a82040004aa8f413813ada9acff4be44d5d
Nullises <texanico@gmail.com>
2017-09-02 23:50:46 -0400

    Clean Status

--- public/assets/js/app.controller.js
+++ public/assets/js/app.controller.js
@@ -35,14 +35,19 @@
 
         });
 
+        $scope.selected = [];
+
         //Función que captura todo del advanced-searchbox
         $scope.$on('advanced-searchbox:modelUpdated', function (event, model) {
           //Objeto key/value con todos los parámetros de búsqueda
           //console.log(model);
 
+          $scope.products = [];
+
           getProductOr.respuesta(model.condition, model.brand, model.name, model.categories)
           .then(function(data){
             console.log(data.data);
+            $scope.products.push(data.data);
           });
 
         });
